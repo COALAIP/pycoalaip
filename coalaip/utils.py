ae9031912de021f9e54d2bf5e264acd75bd5f720
Brett Sun <qisheng.brett.sun@gmail.com>
2016-09-21 11:23:08 +0200

    feature(models): create entity models through factory rather than normal class instantiation
    Supports the use case of loading data lazily from a plugin
    
    BREAKING CHANGE: Breaks all previous entity model instantiations

--- coalaip/utils.py
+++ coalaip/utils.py
@@ -1,3 +1,25 @@
+def data_format_resolver(data_format, resolver_dict):
+    """Resolve a value from :attr:`resolver_dict` based on the
+    :attr:`data_format`.
+
+    Args:
+        data_format (str): the data format; must be one of:
+            - 'jsonld' (default)
+            - 'json'
+            - 'ipld'
+        resolver_dict (dict): the resolving dict. Can hold any value
+            for any of the valid :attr:`data_format` strings
+
+    Returns:
+        the value of the key in :attr:`resolver_dict` that matches
+        :attr:`data_format`
+    """
+    if data_format not in ['jsonld', 'json', 'ipld']:
+        raise ValueError(("'data_format' must be one of 'json', 'jsonld', "
+                          "or 'ipld'. Given '{}'.").format(data_format))
+    return resolver_dict[data_format]
+
+
 # See http://stackoverflow.com/a/26853961/1375656
 def extend_dict(x, *y):
     """Similar to Object.assign() / _.extend() in Javascript, using
